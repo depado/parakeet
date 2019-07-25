4b55e6b182300fdb651ddb9df3991b1c8e3c58bf
Paul Lhussiez <paul.lhussiez@schibsted.com>
2019-07-25 18:32:49 +0200

    Basic ui

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"net/url"
 	"time"
@@ -10,7 +11,8 @@ import (
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/speaker"
-	"github.com/jroimartin/gocui"
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -39,19 +41,11 @@ var versionCmd = &cobra.Command{
 	Run:   func(cmd *cobra.Command, args []string) { fmt.Printf("Build: %s\nVersion: %s\n", Build, Version) }, // nolint: unparam
 }
 
-func layout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	if v, err := g.SetView("title", -1, maxY-2, maxX, maxY); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		fmt.Fprintln(v, "Hello world!")
-	}
-	return nil
-}
-
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
+func formatDuration(t time.Duration) string {
+	h := int64(math.Mod(t.Hours(), 24))
+	m := int64(math.Mod(t.Minutes(), 60))
+	s := int64(math.Mod(t.Seconds(), 60))
+	return fmt.Sprintf("%02d:%02d:%02d", int(h), int(m), int(s))
 }
 
 func run() {
@@ -62,12 +56,11 @@ func run() {
 		logrus.WithError(err).Fatal("Unable to get favorite tracks")
 	}
 	playing := tt[0]
-	fmt.Printf("Reading '%s'\n", playing.Title)
 	o, err := c.Stream(playing)
 	if err != nil {
 		logrus.WithError(err).Fatal("Unable to get stream")
 	}
-	// tot := time.Duration(int64(playing.Duration)) * time.Millisecond
+	tot := time.Duration(int64(playing.Duration)) * time.Millisecond
 
 	resp, err := http.Get(o.HTTPMp3128URL)
 	if err != nil {
@@ -80,39 +73,90 @@ func run() {
 		log.Fatal(err)
 	}
 
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second))
 	done := make(chan bool)
-	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second))
+	ctrl := &beep.Ctrl{Streamer: streamer, Paused: false}
+	speaker.Play(beep.Seq(ctrl, beep.Callback(func() {
 		done <- true
 	})))
 
-	g, err := gocui.NewGui(gocui.OutputNormal)
-	if err != nil {
-		log.Panicln(err)
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
 	}
-	defer g.Close()
+	defer ui.Close()
 
-	g.SetManagerFunc(layout)
+	w, h := ui.TerminalDimensions()
 
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
+	player := widgets.NewGauge()
+	player.Border = false
+	player.Percent = 0
+	player.BarColor = ui.ColorBlue
+	player.LabelStyle = ui.NewStyle(ui.ColorWhite)
+
+	current := widgets.NewParagraph()
+	current.Border = false
+	current.Text = "00:00:00"
+
+	total := widgets.NewParagraph()
+	total.Border = false
+	total.Text = formatDuration(tot)
+
+	current.SetRect(-1, h-2, 9, h+1)
+	player.SetRect(8, h-2, w-9, h+1)
+	total.SetRect(w-9, h-2, w+1, h+1)
+
+	tracklist := widgets.NewList()
+	tracklist.SetRect(-1, 0, w+1, h-1)
+	tracklist.Title = "Tracklist"
+	tracklist.Rows = make([]string, 0, len(tt))
+	for _, t := range tt {
+		tracklist.Rows = append(tracklist.Rows, fmt.Sprintf("%s - %s", t.Title, t.User.Username))
 	}
 
-	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panicln(err)
+	draw := func() {
+		speaker.Lock()
+		cursor := format.SampleRate.D(streamer.Position()).Round(time.Second)
+		speaker.Unlock()
+		player.Label = fmt.Sprintf("%s - %s", playing.Title, playing.User.Username)
+		player.Percent = int(float64(cursor) / float64(tot.Round(time.Second)) * 100)
+		current.Text = formatDuration(cursor)
+		ui.Render(player, current, total, tracklist)
 	}
+	draw()
 
-	time.Sleep(2 * time.Second)
-	// for {
-	// 	select {
-	// 	case <-done:
-	// 		return
-	// 	case <-time.After(time.Second):
-	// 		speaker.Lock()
-	// 		fmt.Printf("\033[2K\r%s / %s", format.SampleRate.D(streamer.Position()).Round(time.Second), tot.Round(time.Second))
-	// 		speaker.Unlock()
-	// 	}
-	// }
+	uiEvents := ui.PollEvents()
+	ticker := time.NewTicker(time.Second).C
+	for {
+		select {
+		case <-done:
+			return
+		case e := <-uiEvents:
+			switch e.ID {
+			case "<Space>":
+				speaker.Lock()
+				ctrl.Paused = !ctrl.Paused
+				speaker.Unlock()
+			case "q", "<C-c>":
+				return
+			case "<Resize>":
+				payload := e.Payload.(ui.Resize)
+				current.SetRect(-1, payload.Height-2, 9, payload.Height+1)
+				player.SetRect(8, payload.Height-2, payload.Width-9, payload.Height+1)
+				total.SetRect(payload.Width-9, payload.Height-2, payload.Width+1, payload.Height+1)
+				tracklist.SetRect(-1, 0, payload.Width+1, payload.Height-1)
+				ui.Clear()
+				draw()
+			}
+			switch e.Type {
+			case ui.KeyboardEvent: // handle all key presses
+				// eventID := e.ID // keypress string
+				// fmt.Println(eventID)
+			}
+		case <-ticker:
+			draw()
+		}
+	}
 }
 
 func main() {
